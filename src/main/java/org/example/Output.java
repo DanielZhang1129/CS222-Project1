ccb5bf5ab0d58aefc74983e8a8dc5c251c3d8205
AlecFeiYe <fei.ye@bsu.edu>
2026-02-09 14:44:16 -0500

    Add printRedirect method to print the redirect message and modify the format of print method.

--- src/main/java/org/example/Output.java
+++ src/main/java/org/example/Output.java
@@ -16,9 +16,14 @@ public class Output {
             System.out.println("No revisions found");
             return;
         }
-
+        int i = 1;
         for (Revision revision : revisions) {
-            System.out.println(revision.getTimestamp() + " " + revision.getUser());
+            System.out.printf("%d: %s %s\n", i, revision.getTimestamp(), revision.getUser());
+            i++;
         }
+    }
+
+    public void printRedirect(String origin, String target) {
+        System.out.println("Redirect: " + origin + " -> " + target);
     }
 }
