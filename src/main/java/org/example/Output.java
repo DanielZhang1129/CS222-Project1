8bf4f64bab63df72cbd186224b99c2e02047437a
AlecFeiYe <fei.ye@bsu.edu>
2026-02-09 12:14:49 -0500

    Implement output formatting for revisions and error messages

--- src/main/java/org/example/Output.java
+++ src/main/java/org/example/Output.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.List;
+
 public class Output {
     public void print(String message) {
         System.out.println(message);
+    }
+
+    public void printErorrMessage(Exception e) {
+        System.out.println(e.getMessage());
+    }
+
+    public void print(List<Revision> revisions) {
+        if (revisions.isEmpty()) {
+            System.out.println("No revisions found");
+            return;
+        }
+
+        for (Revision revision : revisions) {
+            System.out.println(revision.getTimestamp() + " " + revision.getUser());
+        }
     }
 }
