23a7f5d62f6990f0be54cdcd8921b18d3ef64c97
AlecFeiYe <fei.ye@bsu.edu>
2026-02-19 20:44:20 -0500

    Add a click event for search then we can search and print out now!

--- src/main/java/org/example/SearchController.java
+++ src/main/java/org/example/SearchController.java
@@ -1,47 +1,50 @@
 package org.example;
 
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class SearchController {
-    private VBox searchVBox = new VBox(50);
+    private VBox searchVBox;
     private Stage Stage;
 
     public SearchController(Stage Stage) {
         this.Stage = Stage;
+        buildUI();
+    }
+
+    private void buildUI() {
+        searchVBox = new VBox(10);
+        searchVBox.getStyleClass().add("searchVBox");
+        Text text = new Text("Please enter the title: ");
+        text.getStyleClass().add("search-title");
+        TextField searchField = new TextField();
+        TextArea textArea = new TextArea();
+        Button searchButton = new Button("Search");
+
+        Requester requester = new Requester();
+        RevisionParser parser = new RevisionParser();
+        Output output = new Output();
+
+        searchButton.setOnAction(e -> {
+            try {
+                String title = searchField.getText();
+                String json = requester.fetchRecentRevisions(title);
+                String redirect = output.getRedirect(parser.getRedirectTarget(json));
+                String formattedJson = output.getRevisions(parser.parse(json));
+                textArea.setText(redirect + formattedJson);
+            } catch (WikiException a) {
+            }
+        });
+        searchVBox.getChildren().addAll(text, searchField, textArea, searchButton);
     }
 
     public Parent getView() {
         return searchVBox;
     }
-
-//    Input input = new Input();
-//    Output output = new Output();
-//    Requester requester = new Requester();
-//    RevisionParser parser = new RevisionParser();
-//
-//    try
-//
-//    {
-//        String title = input.getUserInput();
-//
-//        String json = requester.fetchRecentRevisions(title);
-//
-//        output.printRedirect(parser.getRedirectTarget(json));
-//
-//        output.printRevisions(parser.parse(json));
-//
-//    } catch(
-//    WikiException e)
-//
-//    {
-//        output.printErrorMessage(e);
-//    }
-
-//    }
-
 }
